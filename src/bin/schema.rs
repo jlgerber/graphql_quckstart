89a4e81cfbefa7c324cc44a3af5b30117eda7099
Jonathan Gerber <jlgerber@gmail.com>
2020-11-08 19:00:38 -0800

    splits out impl into lib

---
+++ src/bin/schema.rs
@@ -0,0 +1,21 @@
+use juniper::{
+    EmptyMutation, 
+    EmptySubscription,
+    IntrospectionFormat,
+};
+use serde_json;
+
+use quickstart::*;
+
+fn main() {
+    let ctx = Database::new();
+    let (res, _errors) = juniper::introspect(
+        &Schema::new(Query, EmptyMutation::new(), EmptySubscription::new()),
+        &ctx,
+        IntrospectionFormat::default(),
+    ).unwrap();
+
+    let json_result = serde_json::to_string_pretty(&res).expect("unable to conver schema to json");
+    println!("{}", json_result);
+
+}
